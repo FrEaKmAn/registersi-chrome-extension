3e8fa6aca13a2f656df5537fa33339aa6a474a8a
Erol <zasebnost@gmail.com>
2015-05-22 21:27:33 +0200

    fixed indents

--- src/popup.ts
+++ src/popup.ts
@@ -8,15 +8,15 @@ class RegisterSI
 
     load():void
     {
-    	this.getCurrentTabUrl((url:string) => 
-	    {
-	        var domain = this.getDomainFromUrl(url);
-	        if(domain)
-	        {
-	            $('#domain').val(domain);
-	        }
-	        this.reloadCaptcha();
-	    });
+        this.getCurrentTabUrl((url:string) => 
+        {
+            var domain = this.getDomainFromUrl(url);
+            if(domain)
+            {
+                $('#domain').val(domain);
+            }
+            this.reloadCaptcha();
+        });
 
         $('.more-information').on('click', () =>
         {
@@ -26,114 +26,114 @@ class RegisterSI
             return false;
         });
 
-	    $('#search').on('submit', () =>
-	    {
-	        $('#available, #captcha-code-error, #domain-error, #results').hide();
+        $('#search').on('submit', () =>
+        {
+            $('#available, #captcha-code-error, #domain-error, #results').hide();
             $('#about').slideUp();
 
-	        var domain:string = this.validateDomain($('#domain').val());
-	        if(domain)
-	        {
-	            $('#loading').show();
-	            this.doWhois(domain);            
-	        }
-	        else
-	        {
-	            $('#domain-error').show();
-	        }
+            var domain:string = this.validateDomain($('#domain').val());
+            if(domain)
+            {
+                $('#loading').show();
+                this.doWhois(domain);            
+            }
+            else
+            {
+                $('#domain-error').show();
+            }
 
-	        return false;
-	    });
+            return false;
+        });
     }
 
-	protected getCurrentTabUrl(callback:(url:string) => void):void
-	{
-	    var queryInfo = {
-	        active: true,
-	        currentWindow: true
-	    };
+    protected getCurrentTabUrl(callback:(url:string) => void):void
+    {
+        var queryInfo = {
+            active: true,
+            currentWindow: true
+        };
 
-	    chrome.tabs.query(queryInfo, (tabs) =>
-	    {
-	        var tab = tabs[0];
-	        var url = tab.url;
-	        console.assert(typeof url == 'string', 'tab.url should be a string');
+        chrome.tabs.query(queryInfo, (tabs) =>
+        {
+            var tab = tabs[0];
+            var url = tab.url;
+            console.assert(typeof url == 'string', 'tab.url should be a string');
 
-	        callback(url);
-	    });
-	}
+            callback(url);
+        });
+    }
 
-	protected getDomainFromUrl(domain:string):string
-	{
-	    // www.register.si > register.si OR subdomain.register.si > register.si
-	    var domain:string = new URL(domain).hostname;
-	    if(!domain)
-	    {
-	        return null;
-	    }
+    protected getDomainFromUrl(domain:string):string
+    {
+        // www.register.si > register.si OR subdomain.register.si > register.si
+        var domain:string = new URL(domain).hostname;
+        if(!domain)
+        {
+            return null;
+        }
 
-	    var parts:string[] = domain.split('.');
-	    if(parts.length < 2)
-	    {
-	        return null;
-	    }
+        var parts:string[] = domain.split('.');
+        if(parts.length < 2)
+        {
+            return null;
+        }
 
-	    if(parts[parts.length-1] !== 'si')
-	    {
-	        return null;
-	    }
+        if(parts[parts.length-1] !== 'si')
+        {
+            return null;
+        }
 
-	    return parts[parts.length-2]; // also subdomain if not www
-	}
+        return parts[parts.length-2]; // also subdomain if not www
+    }
 
-	protected validateDomain(domain:string):string
-	{
-	    domain = $.trim(domain);
-	    if(!domain.length)
-	    {
-	        return null;
-	    }
+    protected validateDomain(domain:string):string
+    {
+        domain = $.trim(domain);
+        if(!domain.length)
+        {
+            return null;
+        }
 
-	    // contains valid symbols [a-zA-Z0-9] + šumniki
-	    return domain;
-	}
+        // contains valid symbols [a-zA-Z0-9] + šumniki
+        return domain;
+    }
 
-	protected reloadCaptcha():void
-	{
+    protected reloadCaptcha():void
+    {
         $('#captcha-code').val('')
-		$('#captcha').attr('src', 'http://www.register.si/index.php?eID=sr_freecap_captcha&id=695');
-	}
+        $('#captcha').attr('src', 'http://www.register.si/index.php?eID=sr_freecap_captcha&id=695');
+    }
 
-	protected doWhois(domain:string):void
-	{
-	    var data = {'domain': domain, 'captcha': $('#captcha-code').val()};
-	    $.post('http://www.register.si/whois.html?no_cache=1', data, (html:string) =>
-	    {
-	        if(html.indexOf('Domena ni registrirana') > -1)
-	        {
-	            $('#available').show();
-	        }
-	        else if(html.indexOf('Napačna koda') > -1)
-	        {
-	            $('#captcha-code-error').show();
-	        }
-	        else
-	        {
-	            html = html.replace(/typo3conf\/ext\/restwhois\/pi1\/picture\.php\?/g, 'http://www.register.si/typo3conf/ext/restwhois/pi1/picture.php?');
-	            html = html.replace(this.imgRegex, '');
-	            var $html = $(html);
-	            var table = $('.tx-restwhois-pi1 > table', $html).html();
-	            $('#results').html(table).show();
-	        }
+    protected doWhois(domain:string):void
+    {
+        var data = {'domain': domain, 'captcha': $('#captcha-code').val()};
+        $.post('http://www.register.si/whois.html?no_cache=1', data, (html:string) =>
+        {
+            if(html.indexOf('Domena ni registrirana') > -1)
+            {
+                $('#available').show();
+            }
+            else if(html.indexOf('Napačna koda') > -1)
+            {
+                $('#captcha-code-error').show();
+            }
+            else
+            {
+                html = html.replace(/typo3conf\/ext\/restwhois\/pi1\/picture\.php\?/g, 'http://www.register.si/typo3conf/ext/restwhois/pi1/picture.php?');
+                html = html.replace(this.imgRegex, '');
+                var $html = $(html);
+                var table = $('.tx-restwhois-pi1 > table', $html).html();
+                $('#results').html(table).show();
+            }
 
-	        this.reloadCaptcha();
-	        $('#loading').hide();
-	    });
-	}
+            this.reloadCaptcha();
+            $('#loading').hide();
+        });
+    }
 }
 
 document.addEventListener('DOMContentLoaded', () => 
 {
-	var registersi = new RegisterSI();
-	registersi.load();
+    var registersi = new RegisterSI();
+    registersi.load();
 });
